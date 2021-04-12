ad1afa793e4e831cabf22e58485cb9e1b8619791
Selfie21 <pierre.brosemer@gmail.com>
2021-04-12 14:11:32 +0200

    added score, win and loss condition

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ var acc_available = true;
 var canvas_height = 0;
 var canvas_width = 0;
 var refreshIntervalId = 0;
+var topmargin = 0;
 
 //Game Variables
 var posx = 0;
@@ -13,6 +14,7 @@ var posy = 0;
 var width = 0;
 var height = 0;
 var difficulty = 0;
+var score = 0;
 
 var clockCounter = 0;
 var rightPressed = false;
@@ -39,6 +41,8 @@ function loadInputs(){
 	canvas_width = Math.round(canvas_height/2);
 	width = Math.round(canvas_width/6);
 	height = 10;
+	topmargin = Math.round(canvas_height/35);
+	score = 0;
 }
 
 // Start Game
@@ -52,29 +56,43 @@ function main() {
 	}, true);
 
 	setupRound(10);
-  	refreshIntervalId = setInterval(draw, 10);
 }
 
-function restart() {
-	clearInterval(refreshIntervalId);
-	setupRound(10);
-	refreshIntervalId = setInterval(draw, 10);
-}
-
-//Resetting Objects
+//Resetting Objects and Starting Round
 function setupRound(diff){
 	posx = Math.round((canvas_width/2) - (width/2));
 	posy = Math.round(canvas_height - 20);
 	for (var i = 0; i < monkeys.length; i++) {
   		var randomPlacement = Math.floor((Math.random() * (canvas_width-width)) + 1);
   		monkeys[i][0] = randomPlacement;
-  		monkeys[i][1] = (i*width);
+  		monkeys[i][1] = (i*width)+topmargin+10;
   		monkeyShow[i] = true;
-	} 
+	}
 	bananax = posx;
 	bananay = posy;
 	difficulty = diff;
+	refreshIntervalId = setInterval(draw, 10);
+}
 
+
+//Checks if each monkey has been shot
+function checkWinCondition(){
+	for (var i = 0; i < monkeys.length; i++) {
+		if(monkeyShow[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+//Checks if monkey touched the ground
+function checkLossCondition(){
+	for (var i = 0; i < monkeys.length; i++) {
+		if(monkeyShow[i] && monkeys[i][1]+width < posy){
+			return false;
+		}
+	}
+	return true;
 }
 
 
@@ -82,12 +100,20 @@ function setupRound(diff){
 function draw() {
 
     ctx.clearRect(0, 0, canvas_width, canvas_height);
+    drawScore();
     drawLead();
     drawBananas();
     drawMonkeys();
 
     checkCollision();
     detectMovement();
+    if(checkWinCondition()){
+		clearInterval(refreshIntervalId);
+		setupRound(Math.round(difficulty/2));
+    }
+    if(checkLossCondition()){
+    	alert("You Lost!")
+    }
 
     if(rightPressed) {
         posx += 1;
@@ -141,17 +167,34 @@ function drawMonkeys(){
 	}
 }
 
+function drawScore(){
+	var test = canvas_height/40;
+	ctx.fillStyle = "black";
+	ctx.font = "bold " + topmargin + "px Helvetica";
+	ctx.fillText("Score:" + score, 7, 20);
+}
 
 function checkCollision(){
 	for (var i = 0; i < monkeys.length; i++) {
 		middlex = bananax+(width/2);
 		middley = bananay+(width/2);
-		if(monkeyShow[i] && (middlex > monkeys[i][0] && middlex < monkeys[i][0]+width) &&  (middley > monkeys[i][1]) && (middley < monkeys[i][1]+width)){
+		if(monkeyShow[i] && inBetween(middlex, monkeys[i][0], width) &&  inBetween(middley, monkeys[i][1], width)){
 			monkeyShow[i] = false;
 			bananaOnScreen = false;
+			score += 1;
 		}
 	}
 }
+
+
+function inBetween(item, edge, length){
+	if((item > edge) && (item < (edge+length))){
+		return true;
+	}
+	return false;
+}
+
+
 // Event Handlers
 function detectMovement(){
 	document.getElementById("acc").innerHTML = "gamma = " + gamma;
