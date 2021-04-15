14ded76694406af57d99678b1e1e56518528dfd7
Selfie21 <pierre.brosemer@gmail.com>
2021-04-15 16:27:21 +0200

    trying to fix device orientation for iOS 12.2+

--- game.js
+++ game.js
@@ -67,6 +67,18 @@ function main() {
 }
 
 
+//requesting Permission for iOS
+function requestDeviceOrientation () {
+  if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+  DeviceOrientationEvent.requestPermission()
+  .then(permissionState => {
+  if (permissionState === 'granted') {
+  window.addEventListener('deviceorientation', () => {});
+  }
+  })
+  .catch(console.error);
+  }
+}
 
 
 
